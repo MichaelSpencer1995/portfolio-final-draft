42d39fc81d61f4a2069bb02af1598d19f99956d1
michael.spencer1995 <michael.spencer1@g.austincc.edu>
2018-05-06 00:23:34 -0500

    converted contact section to styled components

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -4,37 +4,138 @@ import styled from "styled-components"
 class ContactSection extends Component {
   render() {
     return (
-      <div className="contact-section-view">
-        <div className="contact-section-view-container">
-          <h1>
+      <ContactSectionView>
+        <ContactSectionViewContainer>
+          <ContactSectionH1>
             Get in touch
-          </h1>
+          </ContactSectionH1>
 
-          <p>
+          <ContactSectionP>
             I am ready to grind, excel, and push my own limits.
             If you need a competent, willing, and young Javascript developer with UI/UX
             skills, let me prove I can fill that role.
-          </p>
+          </ContactSectionP>
 
-          <div className="form-and-map-container">
-            <div className="form-container">
-              <form>
-                <input placeholder="name" />
-                <input placeholder="number" />
-                <input placeholder="email" />
-                <textarea placeholder="message" />
-                <button className="submit-form-button">
+          <FormAndMapContainer>
+            <FormContainer>
+              <Form>
+                <Input placeholder="name" />
+                <Input placeholder="number" />
+                <Input placeholder="email" />
+                <TextArea placeholder="message" />
+                <FormSubmitButton>
                   Contact
-                </button>
-              </form>
-            </div>
+                </FormSubmitButton>
+              </Form>
+            </FormContainer>
 
-            <iframe className="iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3437.3791666800007!2d-97.65040418526884!3d30.510315781715313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644d0361c2e1531%3A0x13b0645b022fbd90!2sLoquat+Ln%2C+Round+Rock%2C+TX+78664!5e0!3m2!1sen!2sus!4v1525381025642" allowfullscreen></iframe>
-          </div>
-        </div>
-      </div>
+            <Iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3437.3791666800007!2d-97.65040418526884!3d30.510315781715313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644d0361c2e1531%3A0x13b0645b022fbd90!2sLoquat+Ln%2C+Round+Rock%2C+TX+78664!5e0!3m2!1sen!2sus!4v1525381025642" allowfullscreen />
+          </FormAndMapContainer>
+        </ContactSectionViewContainer>
+      </ContactSectionView>
     )
   }
 }
+
+const ContactSectionView = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const ContactSectionViewContainer = styled.div`
+  width: 76%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: -10px;
+  padding-bottom: 85px;
+`
+
+const ContactSectionH1 = styled.h1`
+`
+
+const ContactSectionP = styled.p`
+`
+
+const FormAndMapContainer = styled.div`
+  margin-top: 30px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`
+
+const FormContainer = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`
+
+const Form = styled.form`
+  width: 94%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0;
+  margin: 0;
+`
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  height: 40px;
+  margin-top: 8px;
+  padding-left: 10px;
+  box-sizing: border-box;
+  background: rgb(247, 247, 247);
+  border: #ccc 1px solid;
+  border-radius: 0;
+  -webkit-appearance: none;
+  font-family: 'Open Sans', sans-serif;
+`
+
+const TextArea = styled.textarea`
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  resize: none;
+  height: 190px;
+  margin-top: 10px;
+  padding-left: 8px;
+  padding-top: 6px;
+  box-sizing: border-box;
+  background: rgb(247, 247, 247);
+  border: #ccc 1px solid;
+  border-radius: 0;
+  -webkit-appearance: none;
+  font-family: 'Open Sans', sans-serif;
+`
+
+const FormSubmitButton = styled.button`
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  height: 57px;
+  margin-top: 8px;
+  border-radius: 2px;
+  font-size: 14px;
+  text-shadow: none;
+  background: rgb(33, 149, 147);
+  border: none;
+  font-weight: 500;
+  font-family: 'Open Sans', sans-serif;
+  color: rgba(255, 255, 255, 0.8);
+`
+
+const Iframe = styled.iframe`
+  width: 54%;
+  height: 398px;
+  position: relative;
+  top: 7px;
+  border: #ccc 1px solid;
+`
 
 export default ContactSection
