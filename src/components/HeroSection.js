c544fffd54751c206495739052c59bfc8c12462e
michael.spencer1995 <michael.spencer1@g.austincc.edu>
2018-05-05 23:01:16 -0500

    converted hero to styled components

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import '../App.css'
 import styled from "styled-components"
-import { BrowserView, MobileView, isBrowser, isMobile, isSafari } from "react-device-detect";
+import { isSafari } from "react-device-detect";
 
 let test = 'purple'
 
@@ -12,61 +12,165 @@ if(isSafari){
 class HeroSection extends Component {
   render() {
     return (
-      <div className="hero-section-view">
-        <div className="nav-items">
-          {/* <button className="nav-item">
-            About
-          </button> */}
-
+      <HeroSectionView>
+        <NavItemsContainer>
           <NavItem>
-            Test
+            About
           </NavItem>
           
-          <button className="nav-item">
+          <NavItem>
             Recent Work
-          </button>
+          </NavItem>
 
-          <button className="nav-item">
+          <NavItem>
           Get In Touch
-          </button>
-        </div>
+          </NavItem>
+        </NavItemsContainer>
 
-        <div className="hero-img">
-        </div>
+        <HeroImg />
         
-        <div className="hero-img-overlay">
-        </div>
+        <HeroImgOverlay />
 
-        <div className="hero-content-container">
-          <h1 className="hero-section-h1">
-            My name is <br /><span className="hero-section-h1-span">Michael Spencer</span>          
-          </h1>
+        <HeroContentContainer>
+          <HeroH1>
+            My name is <br /><span>Michael Spencer</span>          
+          </HeroH1>
 
-          <p className="hero-section-p">
+          <HeroP>
             I am a full stack web developer near Austin,
             Texas with very little professional experience,
             eager to help any corporate or personal business
             do better.
-          </p>
+          </HeroP>
 
-          <div className="hire-buttons-container">
-            <button className="hire-buttons">
+          <HireButtonsContainer>
+            <HireButton>
               Hire Me
-            </button>
+            </HireButton>
 
-            <button className="hire-buttons semi-transparent-button">
+            <HireButtonTransparent>
               I need a website
-            </button>
-          </div>
-        </div>
-      </div>
+            </HireButtonTransparent>
+          </HireButtonsContainer>
+        </HeroContentContainer>
+      </HeroSectionView>
     )
   }
 }
 
+const HeroSectionView = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: flex-end;
+`
 
-const NavItem = styled.div`
-  color: ${ test };
+const NavItemsContainer = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  padding: 40px 12% 0px 0px;
+  width: 288px;
+  display: flex;
+  justify-content: space-between;
+  z-index: 100000;
+`
+
+const NavItem = styled.button`
+  font-size: 14px;
+  font-weight: 600;
+  letter-spacing: 0.035rem;
+  text-decoration: none;
+  color: rgba(230, 230, 230, .9);
+  background: transparent;
+  border: none;
+`
+
+const HeroImg = styled.div`
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,0.1);
+  background: url(/assets/img/hero1.png) no-repeat;;
+  background-size: cover;
+  background-position: 20% 90%;
+  background-attachment: fixed;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+`
+
+const HeroImgOverlay = styled.div`
+  background: linear-gradient(rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, .95));
+  border-bottom: rgb(28, 28, 28)1px solid;
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  z-index: 0;
+`
+
+const HeroContentContainer = styled.div`
+  font-size: 18px;
+  letter-spacing: 0.2rem;
+  color: white;
+  margin-left: 12%;
+  margin-bottom: 100px;
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  position: relative;
+`
+
+const HeroH1 = styled.h1`
+  font-size: 30px;
+  font-weight: 100;
+  letter-spacing: 0.05rem;
+  color: rgba(255, 255, 255, 0.75);
+  span{
+    font-size: 50px;
+    font-weight: 600;
+    letter-spacing: 0.04rem;
+    color: rgba(255, 255, 255, 0.9);
+    position: relative;
+    bottom: 7px;
+  }
+`
+
+const HeroP = styled.p`
+  font-size: 15px;
+  font-weight: 300;
+  letter-spacing: -.01rem;
+  line-height: 1.8em;
+  color: #dededef0;
+  margin-top: -18px;
+`
+
+const HireButtonsContainer = styled.div`
+  width: 291px;
+  margin-top: 0px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const HireButton = styled.button`
+  transition: all 0.3s ease;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 12px;
+  font-weight: 500;
+  letter-spacing: 0.01rem;
+  color: rgba(255, 255, 255, 0.8);
+  background: rgb(15, 142, 113);
+  background: rgb(33, 149, 147);
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.15);
+  border: none;
+  border-radius: 4px;
+  width: 142px;
+  height: 31px;
+`
+
+const HireButtonTransparent = styled(HireButton)`
+  background: rgba(181, 181, 181, 0.14);
 `
 
 export default HeroSection
