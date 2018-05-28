37c5b19cdd91d95decf9e19e9ea6079524b3745c
michael.spencer1995 <michael.spencer1@g.austincc.edu>
2018-05-28 12:06:55 -0500

    added env variables

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
+const dotenv = require('dotenv').config()
 const path = require('path')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer')
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 let emailSent = false
 // Generate test SMTP service account from ethereal.email
 // Only needed if you don't have a real mail account for testing
 function sendEmail(body, response){
-    return nodemailer.createTestAccount((err, account) => {
+    nodemailer.createTestAccount((err, account) => {
         let transporter = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
+            host: 'smtp.gmail.com',
             port: 587,
             secure: false,
             auth: {
-                user: 'cpz4kz5zx5dwbhma@ethereal.email',
-                pass: '2utebNGFH1qfebcSU3'
+                user: process.env.usernamegmail,
+                pass: process.env.password
             }
-        });
+        })
 
         // setup email data with unicode symbols
         let mailOptions = {
-            from: `${ body.name } <cpz4kz5zx5dwbhma@ethereal.email>`, // sender addresponses
+            from: `${ body.name } <nodemailerdummyemail@gmail.com>`, // sender addresponses
             to: 'michael.spencer1@g.austincc.edu', // list of receivers
             subject: 'FROM PERSONAL PORTFOLIO', // Subject line
             text: '',
@@ -29,7 +30,7 @@ function sendEmail(body, response){
                    <strong>Phone Number: </strong>${ body.number }<br />
                    <strong>Email: </strong>${ body.email }<br />
                    <strong>Message: </strong>${ body.message }` // html body
-        };
+        }
 
         // send mail with defined transport object
         transporter.sendMail(mailOptions, (error, info) => {
@@ -37,15 +38,15 @@ function sendEmail(body, response){
                 response.status(117).send()
                 return
             }
-            console.log('Message sent: %s', info.messageId);
+            console.log('Message sent: %s', info.messageId)
             response.status(200).send()
             // Preview only available when sending through an Ethereal account
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
 
             // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
             // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-        });
-    });
+        })
+    })
 }
 
 app.use(express.static(path.resolve(__dirname)))
@@ -61,7 +62,7 @@ app.post('/contact', function(req, res){
     // the send email function returns whether or not the email was sent
 })
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
 app.listen(port, function(){
     console.log('connected to port', port)
